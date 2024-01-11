51af81af17e3167756db2fe7d39c1c82f79c35ee
Samuel Chapel <s.chapel@free.fr>
2024-01-11 23:30:08 +0100

    Add Mappers

---
+++ WikY.WebApp/Mappers/ArticleMapper.cs
@@ -0,0 +1,12 @@
+﻿using Riok.Mapperly.Abstractions;
+using WikY.Entities.Articles;
+using WikY.WebApp.Models;
+
+namespace WikY.WebApp.Mappers;
+
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.Target)]
+public partial class ArticleMapper
+{
+    [MapProperty($"{nameof(Article.Author)}.{nameof(Article.Author.FullName)}", nameof(ArticleModel.Author))]
+    public partial ArticleModel Map(Article article);
+}
---
+++ WikY.WebApp/Mappers/AuthorMapper.cs
@@ -0,0 +1,11 @@
+﻿using Riok.Mapperly.Abstractions;
+using WikY.Entities.Authors;
+using WikY.WebApp.Models;
+
+namespace WikY.WebApp.Mappers;
+
+[Mapper(RequiredMappingStrategy = RequiredMappingStrategy.Target)]
+public partial class AuthorMapper
+{
+    public partial AuthorModel Map(Author author);
+}
